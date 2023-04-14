203612c50b9236e61d5cd2842b654c35f697e863
tjdrkr2580 <tjdrkr2580@naver.com>
2023-04-14 14:21:17 +0900

    style : theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import AppLayout from "./AppLayout";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <ThemeProvider theme={theme}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
   );
 }
