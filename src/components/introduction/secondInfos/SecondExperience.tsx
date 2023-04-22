973c7c2f0edb9ddfa7c62e2c67f2de6e574bca69
kimtaehyun <tjdrkr2580@naver.com>
2023-04-22 22:20:47 +0900

    fix : 이미지 변경 node -> nest

--- src/components/introduction/secondInfos/SecondExperience.tsx
+++ src/components/introduction/secondInfos/SecondExperience.tsx
@@ -1,6 +1,5 @@
 import ListTitle from "@/components/global/List";
 import { Line, ListWrapper, StrongTitle } from "@/styles/mixins";
-import styled from "@emotion/styled";
 
 const SecondExperience = () => {
   return (
