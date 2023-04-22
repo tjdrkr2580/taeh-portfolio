54cc997a3fde5c67ab45790b9e99882c9149e2cd
kimtaehyun <tjdrkr2580@naver.com>
2023-04-23 01:48:12 +0900

    style : logoFlyAnimation

--- src/styles/animation/keyframe.ts
+++ src/styles/animation/keyframe.ts
@@ -1,5 +1,23 @@
 import { keyframes } from "@emotion/react";
 
+export const logoFlyAnimation = keyframes`
+    0% {
+        transform: translateY(0);
+    }
+    25% {
+        transform: translateY(10px);
+    }
+    50% {
+        transform: translateY(0);
+    }
+    75% {
+        transform: translateY(10px);
+    }
+    100% {
+        transform: translateY(0);
+    }
+`;
+
 export const logoFillAnimation = keyframes`
     0% {
         fill: transparent;
