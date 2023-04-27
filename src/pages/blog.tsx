aab84fba4718efec1404dfed8a1dbdc3586990f2
kimtaehyun <tjdrkr2580@naver.com>
2023-04-28 02:29:22 +0900

    feat : files 요소 출력

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,26 +1,44 @@
 import HeadInfo from "@/components/global/HeadInfo";
 import useGetAllPost from "@/hook/useGetAllPost";
-import { ErrorWrapper } from "@/styles/mixins";
+import { flexCenterCenter } from "@/styles/mixins";
+import { blogProps, fileProps } from "@/types/props";
 import styled from "@emotion/styled";
 
-const blog = ({ files }: any) => {
-  console.log(files);
+const blog = ({ files }: blogProps) => {
   return (
     <>
       <HeadInfo title="Taeh | 블로그" description="김태현 마크다운 블로그" />
       <BlogWrapper>
-        <ErrorWrapper>
-          현재 마크다운 블로그는 개발 중에 있습니다, 감사합니다
-        </ErrorWrapper>
+        <BlogList>
+          {files.map((file: fileProps, index: number) => (
+            <li key={index}>
+              <h1>{file.data.title}</h1>
+              <p>{file.data.date}</p>
+            </li>
+          ))}
+        </BlogList>
       </BlogWrapper>
     </>
   );
 };
 
 const BlogWrapper = styled.section`
+  min-height: 100vh;
+  ${flexCenterCenter};
+`;
+
+const BlogList = styled.ul`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+  gap: 1rem;
+  justify-content: center;
+
+  li {
+    width: 100%;
+    height: 4rem;
+    padding: 1rem 0;
+  }
 `;
 
 export default blog;
